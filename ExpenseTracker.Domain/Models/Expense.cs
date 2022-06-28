2e9051d3a6290c9ff64a56b1ba3dc110b42c4bc6
law12th <lawrencetepula@gmail.com>
2022-06-28 15:22:46 +0200

    add category and expense models

---
+++ ExpenseTracker.Domain/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Domain.Models {
+    public class Expense {
+
+        [Key]
+        public int ExpenseID { get; set; }
+
+        [Required]
+        public DateTime ExpenseDate { get; set; }
+
+        [Required]
+        public Decimal Amount { get; set; }
+
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category Category { get; set; }
+    }
+}
