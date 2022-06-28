5e69068e1dee2decc6c62d7cd9d067c4a6e79aa6
law12th <lawrencetepula@gmail.com>
2022-06-28 15:28:02 +0200

    add common fields in base model

---
+++ ExpenseTracker.Domain/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpenseTracker.Domain.Models {
+    public class BaseModel {
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateCreated { get; set; }
+
+        [Column(TypeName = "smalldatetime")]
+        public DateTime? DateModified { get; set; }
+    }
+}
